f8fad61352dbdda2ebc46e3207394ca25f4861b1
Aletdownofstate <38739421+Aletdownofstate@users.noreply.github.com>
2024-10-29 16:01:19 +0000

    Player Shooting and animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,37 +7,130 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private Animator animator;
 
+    [SerializeField] private AudioSource gunshotSound;
+    [SerializeField] private Light muzzleFlash;
+
     private float moveHorizontal;
     private float moveVertical;
     private float moveSpeed = 4f;
+
+    private bool isIdle;
+    private bool isRunning = false;
+    private bool isAiming = false;
+
+    public LayerMask enemyLayer;
+    private bool canShoot = true;
+    public float shootRange = 100f;
 
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
         animator = gameObject.GetComponentInChildren<Animator>();
         mainCamera = Camera.main;
+
+        isIdle = true;
+        muzzleFlash.enabled = false;
     }
 
     private void Update()
     {
+        // Inputs
+        // Movement
+
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
+        
+        // Running
 
-        RotateCharacterToMouse();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isRunning = true;
+        }
+        if (!Input.GetKey(KeyCode.LeftShift))
+        {
+            isRunning = false;
+            moveSpeed = 4f;
+        }
+
+        // Aiming
+
+        if (Input.GetMouseButton(1) && !isRunning)
+        {
+            isAiming = true;
+        }
+        if (!Input.GetMouseButton(1) || isRunning)
+        {
+            isAiming = false;
+        }
+
+        // Shooting
+
+        if (Input.GetMouseButton(0) && isAiming && canShoot)
+        {
+            Shoot();
+            StartCoroutine(MuzzleFlashDelay());
+            gunshotSound.Play();
+            StartCoroutine(ShootDelay());
+        }
+
+        RotateCharacterToMouse();       
 
         // Movement animations
+        // Idle
 
         if (moveHorizontal == 0 && moveVertical == 0)
         {
             animator.SetBool("isIdle", true);
             animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", false);
+            isIdle = true;
         }
+
+        // Walking
+
         if (moveVertical != 0 || moveHorizontal != 0)
         {
             animator.SetBool("isWalking", true);
             animator.SetBool("isIdle", false);
+            isIdle = false;
         }
-    }
+
+        // Running
+
+        if (isRunning && rb.velocity.x >= 0.01f || rb.velocity.y >= 0.01f)
+        {
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isIdle", false);
+            isIdle = false;
+
+            moveSpeed = 10f;
+        }
+        else if (!isRunning)
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        // Aiming
+
+        if (isAiming && isIdle)
+        {
+            animator.SetBool("isIdleAiming", true);
+        }
+        else
+        {
+            animator.SetBool("isIdleAiming", false);
+        }
+
+        if (isAiming && !isIdle && !isRunning)
+        {
+            animator.SetBool("isMoveAiming", true);
+        }
+        else
+        {
+            animator.SetBool("isMoveAiming", false);
+        }
+    }    
 
     private void FixedUpdate()
     {
@@ -59,4 +153,30 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
         }
     }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, shootRange, enemyLayer))
+        {
+            // damage to enemy goes here.
+        }
+    }
+
+    private IEnumerator ShootDelay()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(0.25f);
+        canShoot = true;
+    }
+
+    private IEnumerator MuzzleFlashDelay()
+    {
+        muzzleFlash.enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        muzzleFlash.enabled = false;
+    }
+
 }
